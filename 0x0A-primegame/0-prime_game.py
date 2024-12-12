44c414b3d2332d5181c57d8a3093477249a30585
ericvenom99 <prevenakhtar@mail.com>
2024-12-12 18:31:27 +0100

    correct code logic

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,66 +1,59 @@
 #!/usr/bin/python3
-"""
-MODULES DETERMINE THE WINNER IN A PRIME GAME
-"""
+""" Prime Game module """
 
 
 def isWinner(x, nums):
     """
-    Determines the winner of the Prime Game.
+    Determines the winner of a series of rounds of the game.
 
     Args:
-      x: The number of rounds.
-      nums: An array of n, where n is the upper limit for each round.
+    - x (int): The number of rounds.
+    - nums (list): An array of integers representing the upper
+    limit of consecutive integers for each round.
 
     Returns:
-      Player name, or None if no winner can be determined.
+    - str or None: The name of the player that won the most rounds.
+    Returns "Maria" if Maria wins the most rounds, "Ben" if Ben wins
+    the most rounds, and None if the winner cannot be determined.
     """
-    if not x or not nums:
-        return None
-    if x != len(nums):
+    if (type(nums) is not list or not all([type(n) is int for n in nums]) or
+            not all([n > -1 for n in nums])):
         return None
 
-    maria = 0
-    ben = 0
+    if type(x) is not int or x != len(nums):
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
 
     for n in nums:
-        primes = sieve_of_eratosthenes(n)
-        total_primes = len(primes)
-
-        if total_primes % 2 == 0:
-            ben += 1
+        prime_count = sum(1 for i in range(2, n+1) if is_prime(i))
+        if prime_count % 2 == 0:
+            ben_wins += 1
         else:
-            maria += 1
+            maria_wins += 1
 
-    if maria > ben:
+    if maria_wins > ben_wins:
         return "Maria"
-    elif ben > maria:
+    elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
 
 
-def sieve_of_eratosthenes(k):
+def is_prime(num):
     """
-    Finds all prime numbers up to n using the Sieve of Eratosthenes.
+    Checks if a number is prime.
 
     Args:
-        k: The upper limit for finding primes.
+    - num (int): The number to check.
 
     Returns:
-        A list of prime numbers up to n.
+    - bool: True if the number is prime, False otherwise.
     """
-    primes = [True] * (k + 1)
-    primes[0] = primes[1] = False
-
-    for p in range(2, int(k**0.5) + 1):
-        if primes[p]:
-            for i in range(p * p, k + 1, p):
-                primes[i] = False
-
-    prime_numbers = []
-    for p in range(k + 1):
-        if primes[p]:
-            prime_numbers.append(p)
-
-    return prime_numbers
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
